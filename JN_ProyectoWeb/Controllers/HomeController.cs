525c0914ff4758ee7b357bc7b0782404136b75c4
XxpoolxX33 <perezjcarranza@gmail.com>
2025-10-05 15:41:41 -0600

    Semana 3

--- JN_ProyectoWeb/Controllers/HomeController.cs
+++ JN_ProyectoWeb/Controllers/HomeController.cs
@@ -1,27 +1,66 @@
 using System.Diagnostics;
+using JN_ProyectoWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JN_ProyectoWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        #region Actions de Iniciar Sesión
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Index(UsuarioModel usuario)
+        {
+            //ToDo: Validar si existe en la BD
+
+            return View();
+        }
+
+        #endregion
+
+        #region Actions de Registrar Usuario
+
+        [HttpGet]
         public IActionResult Registro()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Registro(UsuarioModel usuario)
+        {
+            //ToDo: registrar la información del usuario en la BD
+
+            return View();
+        }
+
+        #endregion
+
+        #region Actions de Recuperar Acceso
+
+        [HttpGet]
+        public IActionResult RecuperarAcceso()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RecuperarAcceso(UsuarioModel usuario)
+        {
+            //ToDo: validar la información del usuario en la BD y enviarle un correo
+
+            return View();
+        }
+
+        #endregion
+
+        [HttpGet]
         public IActionResult Principal()
         {
             return View();
---
+++ JN_ProyectoWeb/Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+﻿namespace JN_ProyectoWeb.Models
+{
+    public class UsuarioModel
+    {
+        public string Identificacion { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string CorreoElectronico { get; set; } = string.Empty;
+        public string Contrasenna { get; set; } = string.Empty;
+    }
+}
